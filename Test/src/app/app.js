b22a5b1cffd05060186755b2fdc184e0c136c3c2
mtaheri33 <michael.taheri33@gmail.com>
2023-02-14 09:13:58 -0800

    using css file styles

--- Test/src/app/app.js
+++ Test/src/app/app.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
-import ChildAppOne from './children/childAppOne';
+import "./styles.css";
+import ChildAppOne from './components/childAppOne';
 
 class DefaultApp extends Component {
   constructor() {
     super();
-    this.state = {
-      value: 0,
-    };
-  }
-
-  setValue = (newValue) => {
-    this.setState({ value: newValue });
   }
 
   render() {
     return (
       <>
-        <h1>Parent App:</h1>
-        <p>Value: {this.state.value}</p>
-        <ChildAppOne setValue={this.setValue} />
+        <h1>Parent App</h1>
+        <ChildAppOne />
       </>
     )
   }
---
+++ Test/src/app/components/childAppOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let ChildAppOne = () => {
+  return (
+    <>
+      <h2>Child App</h2>
+    </>
+  )
+}
+
+export default ChildAppOne;
