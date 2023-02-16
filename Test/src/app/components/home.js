4b8c8c2fcdf93d41f72cc36c7bb45494987dd7dc
mtaheri33 <michael.taheri33@gmail.com>
2023-02-16 15:10:49 -0800

    handling user input with controlled components

--- Test/src/app/components/home.js
+++ Test/src/app/components/home.js
@@ -4,92 +4,36 @@ class Home extends Component {
   constructor() {
     super();
     this.state = {
-      x: 0,
+      name: null,
+      age: null,
     }
-    this.changeState = this.changeState.bind(this);
+    this.nameInputHandler = this.nameInputHandler.bind(this);
+    this.ageInputHandler = this.ageInputHandler.bind(this);
   }
 
-  changeState() {
-    console.log('changing state');
-    this.setState({ x: 1 });
+  nameInputHandler(event) {
+    this.setState({ name: event.target.value })
   }
 
-  componentDidMount() {
-    console.log('component did mount');
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('checking if component should update');
-    if (nextState.x == 1) {
-      return true;
-    }
-    return false;
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('before state change:');
-    console.log(prevState);
-    return null;
-  }
-
-  componentDidUpdate() {
-    console.log('component did update:');
-    console.log(this.state);
-  }
-
-  componentWillUnmount() {
-    console.log('component unmounting');
+  ageInputHandler(event) {
+    this.setState({ age: event.target.value })
   }
 
   render() {
-    console.log('home render func');
     return (
       <>
         <p>Home</p>
-        <p>state of x: {this.state.x}</p>
-        <button onClick={this.changeState}>Click to change state</button>
+        <label>Name:</label>
+        <input type='text' onChange={this.nameInputHandler}></input>
+        <br></br>
+        <label>Age:</label>
+        <input type='number' onChange={this.ageInputHandler}></input>
+        {/* These will update as characters are typed into the inputs. */}
+        <p>Name: {this.state.name}</p>
+        <p>Age: {this.state.age}</p>
       </>
     )
   }
 }
 
 export default Home;
-
-/*
-render is called when:
-- navigate to page
-- change state
-
-componentDidMount is called when:
-- navigate to page
-
-shouldComponentUpdate is called:
-- right after state change is initiated
-
-getSnapshotBeforeUpdate is called:
-- right after state change is initiated (or right after accepted if using shouldComponentUpdate)
-but before it is committed to DOM
-
-componentDidUpdate is called when:
-- change state is committed to DOM
-
-componentWillUnmount is called when:
-- navigate away from page
-
-So, for the above example, you start the server and navigate to localhost:4000/home
-The console will show:
-home render func
-component did mount
-
-You click the button.  The console will also show:
-changing state
-checking if component should update
-home render func
-before state change:
-{x: 0}
-component did update:
-{x: 1}
-
-You navigate to another page.  The console will also show:
-component unmounting
-*/
