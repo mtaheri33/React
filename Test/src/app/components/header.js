f1b102105c789905ee9301ee137a2583342cc875
mtaheri33 <michael.taheri33@gmail.com>
2023-02-22 09:32:53 -0800

    added username to header to show how store can be accessed from other components and changes when it is updated

--- Test/src/app/components/header.js
+++ Test/src/app/components/header.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = function () {
+const Header = function (props) {
+  const userName = props.User.userName;
   return (
     <header>
       <nav>
         <NavLink to='/home'>Home </NavLink>
         <NavLink to='/about'>About </NavLink>
         <NavLink to='/value'>Value </NavLink>
-        <NavLink to='/user'>User</NavLink>
+        <NavLink to='/user'>User </NavLink>
+        User: {userName}
       </nav>
     </header>
   )
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    User: state.userReducer
+  }
+}
+
+export default connect(mapStateToProps, null)(Header);
